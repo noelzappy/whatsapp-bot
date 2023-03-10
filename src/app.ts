b00f6021196abfb2d097996ae682dd8534eda160
Emmanuel Yeboah <noelzappy@gmail.com>
2023-03-10 02:13:21 +0000

    feat: added initial whatsapp API configurations

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import { whatsapp } from "./config/whatsapp";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  await whatsapp.post("/", {
+    messaging_product: "whatsapp",
+    to: "233542778775",
+    type: "template",
+    template: { name: "hello_world", language: { code: "en_US" } },
+  });
+  res.send("Hello World");
 });
 
 export default app;
---
+++ src/config/whatsapp.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+import config from "./config";
+
+export const whatsapp = axios.create({
+  baseURL: `https://graph.facebook.com/v16.0/${config.wa.phone}/messages`,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${config.wa.token}`,
+  },
+});
